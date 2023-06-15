b268ef34407009974804478ab05f144c5bb847c1
Andrea Occhipinti <andreantoniocchipinti@gmail.com>
2023-06-15 22:02:04 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+
+  const initialState = {
+    // Inizializza lo stato iniziale qui
+  };
+  
+  const [currentState, setState] = useState(initialState);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
